41681754db405d54d9ebffcbf2e5ffd1d8de5632
Magnifis <solomin-2018@mail.ru>
2023-10-01 18:43:42 +0300

    Image was changed

--- src/components/Dialogs/DialogItem/dialogItem.jsx
+++ src/components/Dialogs/DialogItem/dialogItem.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 const DialogItem = (props) => {
   return (
     <div className={tec.dialog}>
-      <img className ={tec.img} src="https://assets.faceit-cdn.net/avatars/80664ec1-a6ec-4473-98a2-ea0c55f952e6_1613496158025.jpg" alt="#" /> 
+      <img className ={tec.img} src="https://coop-land.ru/uploads/posts/2023-02/1677331876_4.jpg" alt="#" /> 
       <NavLink className={tec.nam} to={"/dialog/" + props.id}>{props.name}</NavLink>
     </div>
   );
